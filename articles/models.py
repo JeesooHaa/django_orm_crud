9c5e3ab268abb32e661ec17d00e4ddb1abb4368e
JeesooHaa <haajeesoo@gmail.com>
2019-08-22 12:08:25 +0900

    Create README.md
     - Learn CRUD Operation with Django ORM

--- articles/models.py
+++ articles/models.py
@@ -5,8 +5,15 @@ class Article(models.Model):
     # id(pk)는 기본적으로 처음 테이블 생성시 자동으로 만들어 진다.
     # 따라서 만들 필요가 없음...
     # id = models.AutoField(primary_key=True) 
+
+    # 모든 필드는 기본저으로 NOT NULL => 비어있으면 안된다. 
     
     # CharField 에서는 max_length 가 필수 인자다. 
     title = models.CharField(max_length=20)  # 클래스 변수 (DB의 필드)
     content = models.TextField()  # 클래스 변수 (DB의 필드)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)  # add 추가됬을때
+    updated_at = models.DateTimeField(auto_now=True)  # 수정되었을때 (언제든지)
+
+    def __str__(self):
+        return f'{self.id}번 글 - {self.title} : {self.content}'
+    
--- django_orm.md
+++ django_orm.md
@@ -1,3 +1,5 @@
+### Day01
+
 언어 통합?
 
 아무튼 세팅 바꾸면 다른 DB 다 쓸 수 있다. 
@@ -22,7 +24,35 @@ ORM - sql query / 근데 알아서 처리해줌
 
 
 
-$ python manage.py makemigrations
+$ python manage.py makemigrations // django 한테 model 작성했음을 알림
 
-$ python manage.py migrate
+$ python manage.py migrate // django 한테 실제 DB 에 작성하라고 명령 
+
+
+
+### Day02
+
+1. model 을 작성한다.
+2. django 에 알려준다. makemigrations
+3. 실제 DB 에 반영한다. migrate
+
+
+
+django project 환경에서 python shell 을 사용한다.
+
+```bash
+$ python manage.py shell
+```
+
+article 을 import  해야 사용가능 
+
+```shell
+from articles.models import Article 
+```
+
+model 에 접근하는 명령어?
+
+```python
+<model>.objects
+```
 
